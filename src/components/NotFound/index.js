afef672d49612852883f773259585ccab7e6e6cc
Saipavan447 <pavankrishna529@gmail.com>
2022-05-14 18:09:26 +0000

    jobbyApp

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found">
+    <div className="not-card">
+      <img
+        className="not-found-img"
+        src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+        alt="not found"
+      />
+      <h1 className="not-found-heading"> Page Not Found</h1>
+      <p className="not-found-description">
+        we're sorry, the page you requested could not be found
+      </p>
+    </div>
+  </div>
+)
+export default NotFound
